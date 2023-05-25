4fb5f72bbf2e0b7649058cd44d324d82a485728d
leo78913 <leo3758@riseup.net>
2023-05-25 10:47:56 -0300

    fix: fix client mixin

--- src/main/java/com/github/redsonbr140/wayland/mixin/MixinMinecraftClient.java
+++ src/main/java/com/github/redsonbr140/wayland/mixin/MixinMinecraftClient.java
@@ -1,15 +1,18 @@
 package com.github.redsonbr140.wayland.mixin;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.client.MinecraftClient;
+
+import java.io.InputStream;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftClient {
-@Redirect(method = "<init>*", at = @At(value = "INVOKE", target = "L/net/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
-private void redirectSetIcon() {
-	// Do nothing (redirected method call is skipped)
-}
+	@Redirect(method = "<init>*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
+	private void redirectSetIcon(Window window, InputStream icon16, InputStream icon32) {
+		// Do nothing (redirected method call is skipped)
+	}
 }
